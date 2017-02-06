383e1cf0c99c6209006b22ec2ebed56fe798dde0
miragoon <miragoon@hotmail.com>
2017-02-05 22:58:00 -0500

    Finish lesson 1

--- app/src/main/java/com/cokecat/mirag/sunshinemaly/MainFragment.java
+++ app/src/main/java/com/cokecat/mirag/sunshinemaly/MainFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,7 +28,28 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> entries = new ArrayList<>();
+        entries.add("Today - Sunny - 88/63");
+        entries.add("Tomorrow - Foggy - 70/40");
+        entries.add("Weds - Cloudy - 72/63");
+        entries.add("Thurs - Rainy - 64/51");
+        entries.add("Fri - Foggy - 70/46");
+        entries.add("Sat - Sunny - 76/68");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                entries
+        );
+
+        ListView mListView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        mListView.setAdapter(adapter);
+
+
+        return rootView;
     }
 
 }
